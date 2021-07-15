b37d832b781dc527dd73d3dceb59c4b4a561243d
irinejiss <87313540+irinejiss@users.noreply.github.com>
2021-07-15 15:45:35 +0530

    changes

--- task/ViewController.swift
+++ task/ViewController.swift
@@ -10,27 +10,6 @@ import UIKit
 class ViewController: UIViewController {
     
     var isActive : Bool = false
-    
-    @IBOutlet weak var clickin: UIButton!
-    @IBAction func click(_ sender: AnyObject) {
-        if isActive {
-            isActive = false
-            clickin.setImage(UIImage(named: "completed"), for: .normal)
-            imagePic.image = UIImage(named: "success")
-            label.text = "Completed"
-        }
-        else{
-            isActive = true
-            clickin.setImage(UIImage(named: "pending"), for: .normal)
-            imagePic.image = UIImage(named: "Image")
-            label.text = "Pending"
-        }
-        
-    }
-    
-    @IBOutlet weak var imagePic: UIImageView!
-    
-    @IBOutlet weak var label: UILabel!
     override func viewDidLoad() {
             super.viewDidLoad()
             // Do any additional setup after loading the view, typically from a nib.
@@ -40,6 +19,21 @@ class ViewController: UIViewController {
             super.didReceiveMemoryWarning()
             // Dispose of any resources that can be recreated.
     }
-
-
-}
+    @IBOutlet weak var button: UIButton!
+    @IBOutlet weak var imagePic: UIImageView!
+    @IBOutlet weak var label: UILabel!
+    @IBAction func bittonPressed(_ sender: AnyObject) {
+        if isActive {
+            isActive = false
+            button.setImage(UIImage(named: "completed"), for: .normal)
+            imagePic.image = UIImage(named: "success")
+            label.text = "Completed"
+        }
+        else{
+            isActive = true
+            button.setImage(UIImage(named: "pending"), for: .normal)
+            imagePic.image = UIImage(named: "Image")
+            label.text = "Pending"
+        }
+        }
+    }
