4c0a7af5240ee911f4dbfb0e0443b807b55043c5
irinejiss <87313540+irinejiss@users.noreply.github.com>
2021-07-15 16:26:09 +0530

    constraints changed

--- task/ViewController.swift
+++ task/ViewController.swift
@@ -9,31 +9,30 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var isActive : Bool = false
-    override func viewDidLoad() {
-            super.viewDidLoad()
-            // Do any additional setup after loading the view, typically from a nib.
-        }
-
-    override func didReceiveMemoryWarning() {
-            super.didReceiveMemoryWarning()
-            // Dispose of any resources that can be recreated.
-    }
-    @IBOutlet weak var button: UIButton!
+    @IBOutlet weak var checkButton: UIButton!
     @IBOutlet weak var imagePic: UIImageView!
-    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var statusLabel: UILabel!
+    
+    var isActive : Bool = false
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        }
+    
     @IBAction func bittonPressed(_ sender: AnyObject) {
         if isActive {
             isActive = false
-            button.setImage(UIImage(named: "completed"), for: .normal)
+            
+            checkButton.setBackgroundImage(UIImage(named: "completed"), for: .normal)
             imagePic.image = UIImage(named: "success")
-            label.text = "Completed"
+            statusLabel.text = "Completed"
         }
         else{
             isActive = true
-            button.setImage(UIImage(named: "pending"), for: .normal)
+            checkButton.setBackgroundImage(UIImage(named: "pending"), for: .normal)
             imagePic.image = UIImage(named: "Image")
-            label.text = "Pending"
-        }
+            statusLabel.text = "Pending"
         }
     }
+}
